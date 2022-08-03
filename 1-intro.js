848642758f8a9a91210ac9fcb21c23c9c13e1aa5
VanadiumV <93636758+VanadiumV@users.noreply.github.com>
2022-08-04 03:04:19 +0530

    Add files via upload

---
+++ 1-intro.js
@@ -0,0 +1,14 @@
+//GLOBALS -NO WINDOW
+//__dirname-path to current directory
+//_filename-file name
+//require -function to use modules(CommonJS)
+//module-info about current module (file)
+//process- info about env where the program is being executed
+const amount = 12
+if  (amount < 10 ){
+    console.log('small number')
+}else{
+    console.log('large number')
+}
+
+console.log('hey its my first node app!!!' )
---
+++ 3-modules.js
@@ -0,0 +1,11 @@
+//CommonJS,every file is module (by default)
+//MODULES -Encapsulated Code (only share minimum)
+const names = require('./4-NAMESmodule1')
+const sayHi = require('./5-utilis')
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+ //console.log(data)
+
+ // sayHi('susan')
+ // sayHi(names.john)
+ // sayHi(names.peter)
